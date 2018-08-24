eb072560e40c77cec1b0c8cd7a023aac43b3edd4
agamemnonc <agamemnon.krasoulis@gmail.com>
2018-08-24 17:53:20 +0100

    new (multiclass-multioutput) metrics and tests

--- sklearn_ext/metrics.py
+++ sklearn_ext/metrics.py
@@ -7,11 +7,20 @@ from scipy.sparse import csr_matrix
 
 from sklearn import metrics
 from sklearn.utils.multiclass import type_of_target
+from sklearn.utils.multiclass import unique_labels
 from sklearn.utils import check_consistent_length
 from sklearn.utils import column_or_1d
+from sklearn.utils.sparsefuncs import count_nonzero
+from sklearn.utils.validation import _num_samples
 
 
-__all__ = ['multiclass_multioutput']
+__all__ = [
+    'accuracy_score',
+    'hamming_score',
+    'zero_one_loss',
+    'hamming_loss',
+    'multiclass_multioutput'
+]
 
 
 def _check_targets(y_true, y_pred):
@@ -70,7 +79,7 @@ def _check_targets(y_true, y_pred):
     return y_type, y_true, y_pred
 
 
-def weighted_sum(sample_score, weight, normalize=False):
+def _weighted_sum(sample_score, weight, normalize=False):
     """Computes a weighted sum"""
     if normalize:
         # np.average can handle the case where weight=None
@@ -81,9 +90,257 @@ def weighted_sum(sample_score, weight, normalize=False):
         return sample_score.sum()
 
 
+def accuracy_score(y_true, y_pred, normalize=True, sample_weight=None):
+    """Accuracy classification score.
+    In multilabel classification, this function computes subset accuracy:
+    the set of labels predicted for a sample must *exactly* match the
+    corresponding set of labels in y_true.
+    Read more in the :ref:`User Guide <accuracy_score>`.
+    Parameters
+    ----------
+    y_true : 1d array-like, or label indicator array / sparse matrix
+        Ground truth (correct) labels.
+    y_pred : 1d array-like, or label indicator array / sparse matrix
+        Predicted labels, as returned by a classifier.
+    normalize : bool, optional (default=True)
+        If ``False``, return the number of correctly classified samples.
+        Otherwise, return the fraction of correctly classified samples.
+    sample_weight : array-like of shape = [n_samples], optional
+        Sample weights.
+    Returns
+    -------
+    score : float
+        If ``normalize == True``, return the correctly classified samples
+        (float), else it returns the number of correctly classified samples
+        (int).
+        The best performance is 1 with ``normalize == True`` and the number
+        of samples with ``normalize == False``.
+    See also
+    --------
+    hamming_score, hamming_loss, zero_one_loss
+    Notes
+    -----
+    In binary and multiclass classification, this function is equal
+    to the ``hamming_score`` function.
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from sklearn_ext.metrics import accuracy_score
+    >>> y_pred = [0, 2, 1, 3]
+    >>> y_true = [0, 1, 2, 3]
+    >>> accuracy_score(y_true, y_pred)
+    0.5
+    >>> accuracy_score(y_true, y_pred, normalize=False)
+    2
+    In the multilabel case with binary label indicators:
+    >>> accuracy_score(np.array([[0, 1], [1, 1]]), np.ones((2, 2)))
+    0.5
+    """
+
+    # Compute accuracy for each possible representation
+    y_type, y_true, y_pred = _check_targets(y_true, y_pred)
+    check_consistent_length(y_true, y_pred, sample_weight)
+    if y_type.startswith(('multilabel', 'multiclass-multioutput')):
+        differing_labels = np.count_nonzero(y_true - y_pred, axis=1)
+        score = differing_labels == 0
+    else:
+        score = y_true == y_pred
+
+    return _weighted_sum(score, sample_weight, normalize)
+
+
+def zero_one_loss(y_true, y_pred, normalize=True, sample_weight=None):
+    """Zero-one classification loss.
+    If normalize is ``True``, return the fraction of misclassifications
+    (float), else it returns the number of misclassifications (int). The best
+    performance is 0.
+    Read more in the :ref:`User Guide <zero_one_loss>`.
+    Parameters
+    ----------
+    y_true : 1d array-like, or label indicator array / sparse matrix
+        Ground truth (correct) labels.
+    y_pred : 1d array-like, or label indicator array / sparse matrix
+        Predicted labels, as returned by a classifier.
+    normalize : bool, optional (default=True)
+        If ``False``, return the number of misclassifications.
+        Otherwise, return the fraction of misclassifications.
+    sample_weight : array-like of shape = [n_samples], optional
+        Sample weights.
+    Returns
+    -------
+    loss : float or int,
+        If ``normalize == True``, return the fraction of misclassifications
+        (float), else it returns the number of misclassifications (int).
+    Notes
+    -----
+    In multilabel classification, the zero_one_loss function corresponds to
+    the subset zero-one loss: for each sample, the entire set of labels must be
+    correctly predicted, otherwise the loss for that sample is equal to one.
+    See also
+    --------
+    accuracy_score, hamming_loss, jaccard_similarity_score
+    Examples
+    --------
+    >>> from sklearn_ext.metrics import zero_one_loss
+    >>> y_pred = [1, 2, 3, 4]
+    >>> y_true = [2, 2, 3, 4]
+    >>> zero_one_loss(y_true, y_pred)
+    0.25
+    >>> zero_one_loss(y_true, y_pred, normalize=False)
+    1
+    In the multilabel case with binary label indicators:
+    >>> zero_one_loss(np.array([[0, 1], [1, 1]]), np.ones((2, 2)))
+    0.5
+    """
+    score = accuracy_score(y_true, y_pred,
+                           normalize=normalize,
+                           sample_weight=sample_weight)
+
+    if normalize:
+        return 1 - score
+    else:
+        if sample_weight is not None:
+            n_samples = np.sum(sample_weight)
+        else:
+            n_samples = _num_samples(y_true)
+        return n_samples - score
+
+def hamming_score(y_true, y_pred, labels=None, sample_weight=None):
+    """Hamming classification score.
+    In multilabel classification, this function computes label-based accuracy.
+    Read more in the :ref:`User Guide <accuracy_score>`.
+    Parameters
+    ----------
+    y_true : 1d array-like, or label indicator array / sparse matrix
+        Ground truth (correct) labels.
+    y_pred : 1d array-like, or label indicator array / sparse matrix
+        Predicted labels, as returned by a classifier.
+    labels : array, shape = [n_labels], optional (default=None)
+        Integer array of labels. If not provided, labels will be inferred
+        from y_true and y_pred.
+        .. versionadded:: 0.18
+    sample_weight : array-like of shape = [n_samples], optional
+        Sample weights.
+    Returns
+    -------
+    score : float
+        If ``normalize == True``, return the correctly classified samples
+        (float), else it returns the number of correctly classified samples
+        (int).
+        The best performance is 1 with ``normalize == True`` and the number
+        of samples with ``normalize == False``.
+    See also
+    --------
+    accuracy_score, hamming_loss, zero_one_loss
+    Notes
+    -----
+    In binary and multiclass classification, this function is equal
+    to the ``accuracy_score`` function.
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from sklearn_ext.metrics import hamming_score
+    >>> y_pred = [0, 2, 1, 3]
+    >>> y_true = [0, 1, 2, 3]
+    >>> hamming_score(y_true, y_pred)
+    0.5
+    >>> hamming_score(y_true, y_pred, normalize=False)
+    2
+    In the multilabel case with binary label indicators:
+    >>> hamming_score(np.array([[0, 1], [1, 1]]), np.ones((2, 2)))
+    0.75
+    """
+    loss = hamming_loss(y_true, y_pred,
+                           labels=labels,
+                           sample_weight=sample_weight)
+    return 1 - loss
+
+
+def hamming_loss(y_true, y_pred, labels=None, sample_weight=None):
+    """Compute the average Hamming loss.
+    The Hamming loss is the fraction of labels that are incorrectly predicted.
+    Read more in the :ref:`User Guide <hamming_loss>`.
+    Parameters
+    ----------
+    y_true : 1d array-like, or label indicator array / sparse matrix
+        Ground truth (correct) labels.
+    y_pred : 1d array-like, or label indicator array / sparse matrix
+        Predicted labels, as returned by a classifier.
+    labels : array, shape = [n_labels], optional (default=None)
+        Integer array of labels. If not provided, labels will be inferred
+        from y_true and y_pred.
+        .. versionadded:: 0.18
+    sample_weight : array-like of shape = [n_samples], optional
+        Sample weights.
+        .. versionadded:: 0.18
+    Returns
+    -------
+    loss : float or int,
+        Return the average Hamming loss between element of ``y_true`` and
+        ``y_pred``.
+    See Also
+    --------
+    accuracy_score, jaccard_similarity_score, zero_one_loss
+    Notes
+    -----
+    In multiclass classification, the Hamming loss correspond to the Hamming
+    distance between ``y_true`` and ``y_pred`` which is equivalent to the
+    subset ``zero_one_loss`` function.
+    In multilabel classification, the Hamming loss is different from the
+    subset zero-one loss. The zero-one loss considers the entire set of labels
+    for a given sample incorrect if it does entirely match the true set of
+    labels. Hamming loss is more forgiving in that it penalizes the individual
+    labels.
+    The Hamming loss is upperbounded by the subset zero-one loss. When
+    normalized over samples, the Hamming loss is always between 0 and 1.
+    References
+    ----------
+    .. [1] Grigorios Tsoumakas, Ioannis Katakis. Multi-Label Classification:
+           An Overview. International Journal of Data Warehousing & Mining,
+           3(3), 1-13, July-September 2007.
+    .. [2] `Wikipedia entry on the Hamming distance
+           <https://en.wikipedia.org/wiki/Hamming_distance>`_
+    Examples
+    --------
+    >>> from sklearn_ext.metrics import hamming_loss
+    >>> y_pred = [1, 2, 3, 4]
+    >>> y_true = [2, 2, 3, 4]
+    >>> hamming_loss(y_true, y_pred)
+    0.25
+    In the multilabel case with binary label indicators:
+    >>> hamming_loss(np.array([[0, 1], [1, 1]]), np.zeros((2, 2)))
+    0.75
+    """
+
+    y_type, y_true, y_pred = _check_targets(y_true, y_pred)
+    check_consistent_length(y_true, y_pred, sample_weight)
+
+    if sample_weight is None:
+        weight_average = 1.
+    else:
+        weight_average = np.mean(sample_weight)
+
+    if y_type.startswith('multilabel'):
+
+        if labels is None:
+            labels = unique_labels(y_true, y_pred)
+        else:
+            labels = np.asarray(labels)
+
+        n_differences = count_nonzero(y_true - y_pred,
+                                      sample_weight=sample_weight)
+        return (n_differences /
+                (y_true.shape[0] * len(labels) * weight_average))
+
+    elif y_type in ["binary", "multiclass", "multiclass-multioutput"]:
+        return _weighted_sum(y_true != y_pred, sample_weight, normalize=True)
+    else:
+        raise ValueError("{0} is not supported".format(y_type))
+
+
 def multiclass_multioutput(metric, y_true, y_pred, normalize=True,
                            sample_weight=None, class_average='binary',
-                           output_weight=None, output_normalize=None):
+                           output_weight=None, output_normalize=True):
     """Extends classification metrics to support multiclass and multilabel
 
     The metric is calculated for each output independently (i.e. the metric
@@ -150,8 +407,8 @@ def multiclass_multioutput(metric, y_true, y_pred, normalize=True,
     y_pred : array or indicator matrix
     """
     if metric not in ['accuracy_score', 'zero_one_loss', 'log_loss',
-                      'jaccard_similarity_score', 'precision_score',
-                      'recall_score', 'f1_score']:
+                      'hamming_loss', 'jaccard_similarity_score',
+                      'precision_score', 'recall_score', 'f1_score']:
         raise ValueError("{} score not supported.".format(metric))
 
     score_function = getattr(metrics, metric)
@@ -193,4 +450,4 @@ def multiclass_multioutput(metric, y_true, y_pred, normalize=True,
                 y_true=y_true[:, output].reshape(num_samples, -1),
                 y_pred=y_pred[:, output].reshape(num_samples, -1),
                 average=class_average, sample_weight=sample_weight)
-    return weighted_sum(scores, output_weight, output_normalize)
+    return _weighted_sum(scores, output_weight, output_normalize)
---
+++ sklearn_ext/tests/test_metrics.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+from sklearn.utils.testing import assert_almost_equal
+
+from sklearn.datasets import make_multilabel_classification
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+from sklearn_ext.metrics import accuracy_score, zero_one_loss
+from sklearn_ext.metrics import hamming_loss, hamming_score
+from sklearn_ext.metrics import multiclass_multioutput
+
+
+# Data
+Y1 = np.array([[1, 0],
+               [2, 2],
+               [0, 1],
+               [0, 2],
+               [0, 1]])
+
+Y2 = np.array([[1, 1],
+               [2, 2],
+               [0, 1],
+               [0, 2],
+               [0, 1]])
+
+# Multi-class multi-label data
+X, Y = make_multilabel_classification(n_samples=1000,
+                                      n_features=30,
+                                      n_classes=5,
+                                      random_state=606)
+Y = np.column_stack((Y, Y[:, 0] + Y[:, 1], Y[:, 3] + Y[:, 2]))
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2,
+                                                    random_state=606)
+clf = RandomForestClassifier(n_estimators=100)
+clf.fit(X_train, Y_train)
+Y_pred = clf.predict(X_test)
+Y_true = Y_test
+
+
+def test_accuracy_score():
+    assert_almost_equal(accuracy_score(Y1, Y2), 0.8)
+
+
+def test_zero_one_loss():
+    assert_almost_equal(zero_one_loss(Y1, Y2), 0.2)
+
+
+def test_hamming_score():
+    assert_almost_equal(hamming_score(Y1, Y2), 0.9)
+
+
+def test_hamming_loss():
+    assert_almost_equal(hamming_loss(Y1, Y2), 0.1)
+    assert_almost_equal(multiclass_multioutput('hamming_loss', Y1, Y2),
+                        hamming_loss(Y1, Y2))
+
+
+def test_accuracy_zero_one_loss():
+    assert_almost_equal(accuracy_score(Y_true, Y_pred) +
+                        zero_one_loss(Y_true, Y_pred), 1)
+
+
+def test_hamming_loss_score():
+    assert_almost_equal(hamming_score(Y_true, Y_pred) +
+                        hamming_loss(Y_true, Y_pred), 1)
