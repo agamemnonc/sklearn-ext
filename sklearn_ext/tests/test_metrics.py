68c81e4c89d2d9244a8ee106b1736105c7d43a2e
agamemnonc <agamemnon.krasoulis@gmail.com>
2018-11-01 09:44:53 +0000

    test for multiclass_multioutput log_loss

--- sklearn_ext/tests/test_metrics.py
+++ sklearn_ext/tests/test_metrics.py
@@ -10,6 +10,7 @@ from sklearn_ext.metrics import accuracy_score, zero_one_loss
 from sklearn_ext.metrics import hamming_loss, hamming_score
 from sklearn_ext.metrics import multiclass_multioutput
 
+random_state = 606
 
 # Data
 Y1 = np.array([[1, 0],
@@ -28,11 +29,11 @@ Y2 = np.array([[1, 1],
 X, Y = make_multilabel_classification(n_samples=1000,
                                       n_features=30,
                                       n_classes=5,
-                                      random_state=606)
+                                      random_state=random_state)
 Y = np.column_stack((Y, Y[:, 0] + Y[:, 1], Y[:, 3] + Y[:, 2]))
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2,
-                                                    random_state=606)
-clf = RandomForestClassifier(n_estimators=100)
+                                                    random_state=random_state)
+clf = RandomForestClassifier(n_estimators=100, random_state=random_state)
 clf.fit(X_train, Y_train)
 Y_pred = clf.predict(X_test)
 Y_true = Y_test
@@ -64,3 +65,7 @@ def test_accuracy_zero_one_loss():
 def test_hamming_loss_score():
     assert_almost_equal(hamming_score(Y_true, Y_pred) +
                         hamming_loss(Y_true, Y_pred), 1)
+
+def test_log_loss():
+    assert_almost_equal(multiclass_multioutput('log_loss', Y_true, clf.predict_proba(X_test)),
+                        0.6454764507804741)
