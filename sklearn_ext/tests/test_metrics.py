924163a161bd05671ca34f72b8726fb5faba43da
agamemnonc <agamemnon.krasoulis@gmail.com>
2019-01-15 17:29:04 +0000

    multiclass_multioutput fix argument order

--- sklearn_ext/tests/test_metrics.py
+++ sklearn_ext/tests/test_metrics.py
@@ -53,7 +53,7 @@ def test_hamming_score():
 
 def test_hamming_loss():
     assert_almost_equal(hamming_loss(Y1, Y2), 0.1)
-    assert_almost_equal(multiclass_multioutput('hamming_loss', Y1, Y2),
+    assert_almost_equal(multiclass_multioutput(Y1, Y2, 'hamming_loss', ),
                         hamming_loss(Y1, Y2))
 
 
@@ -70,7 +70,7 @@ def test_hamming_loss_score():
 def test_log_loss():
     assert_almost_equal(
         multiclass_multioutput(
-            'log_loss',
             Y_true,
-            clf.predict_proba(X_test)),
+            clf.predict_proba(X_test),
+            'log_loss',),
         0.6454764507804741)
